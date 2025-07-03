c7c46218008eec0eeb70f981337b11046b582c49
Shahriar Ahmed ~ SHIFAT <shahriar.ahmed.7878@gmail.com>
2025-07-03 14:22:48 +0600

    Refactor pattern printing logic in pattern_7.cpp to simplify output

--- 1_first/pattern_7.cpp
+++ 1_first/pattern_7.cpp
@@ -8,10 +8,11 @@ int main(){
     int row=1;
     while(row<=n){
         int col=1;
-        int val=row;
+        // int val=row;
         while(col<=row){
-            cout<<" "<<val<<" ";
-            val--;
+            // cout<<" "<<val<<" ";
+            // val--;
+            cout<<" "<<row-col+1<<" ";
             col++;
         }
         cout<<endl;
