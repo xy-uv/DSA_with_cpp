55b1f9e659738a8641624bab561c9bfb1816da94
Shahriar Ahmed ~ SHIFAT <shahriar.ahmed.7878@gmail.com>
2025-07-04 20:13:17 +0600

    Add pattern printing programs in pattern_19.cpp, pattern_20.cpp, pattern_21.cpp, pattern_22.cpp, and pattern_23.cpp

---
+++ 1_first/pattern_23.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    int row=1,count=1;
+    while(row<=n){
+        int space=row-1;
+        while(space){
+            cout<<"   ";
+            space--;
+        }
+        int col=1;
+        while(col<=(n-row+1)){
+            cout<<" "<<count<<" ";
+            count++;
+            col++;
+        }
+        cout<<endl;
+        row++;
+    }
+}
