4d328a9500da579a6cb8f8a3096c8e855da50970
Shahriar Ahmed ~ SHIFAT <shahriar.ahmed.7878@gmail.com>
2025-07-24 13:55:34 +0600

    Array-> Linear Search

---
+++ 6_array/linear_search.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+
+bool search(int arr[],int size,int key){
+    for(int i=0;i<=size;i++){
+        if(key==arr[i]){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main(){
+    int arr[99] = {
+        8, 9, 22, 92, 15, 34, 67, 221, 73, 14,
+        5, 19, 38, 45, 56, 60, 77, 83, 91, 24,
+        6, 11, 13, 17, 20, 25, 30, 333, 36, 39,
+        42, 44, 46, 487, 50, 52, 54, 58, 61, 63,
+        657, 687, 780, 872, 74, 76, 79, 81, 874, 86,
+        88, 790, 93, 95, 967, 999, 1, 82, 3, 4,
+        7, 10, 12, 16, 168, 23, 276, 27, 28, 29,
+        31, 32, 356, 37, 40, 41, 43, 470, 49, 51,
+        53, 55, 57, 599, 62, 64, 66, 69, 71, 795,
+        78, 80, 82, 85, 87, 899, 94, 96, 988
+    };
+    // cout<<&arr<<endl;
+    cout<<"Enter the key/value that's you want to find: ";
+    int k;
+    cin>>k;
+
+    if(search(arr,99,k)){
+        cout<<"Key "<<k<<" retrieve successfully"<<endl;
+    }else{
+        cout<<"Key "<<k<<" aren't available on the series."<<endl;
+    }
+}
